005e6d29214bbbf87c87bea72821a75d22263e9f
DeriDreri <dominikcybulski@protonmail.com>
2024-07-19 12:44:55 +0200

    Implement debug version of the app
    - Added macros for printing debug info to STDERR
    - Implemented debug task in Makefile

--- Makefile
+++ Makefile
@@ -6,19 +6,24 @@ CC = gcc
 CFLAGS = -Wall -Wextra -pedantic
 
 help:
-	@echo '---------------------------------------------------'
+	@echo '------------------------------------------------------'
 	@echo "make buld - build the project"
 	@echo "make run - build and run the project"
+	@echo "make debug - build and run the project in debug mode"
 	@echo "make clean - clean the project"
-	@echo '---------------------------------------------------'
+	@echo '------------------------------------------------------'
+
+debug: CFLAGS += -DDEBUG -g
+debug: $(TARGET_DIR)/$(TARGET)
+	@./$(TARGET_DIR)/$(TARGET)
 
 build: $(TARGET_DIR)/$(TARGET)
 
 run: $(TARGET_DIR)/$(TARGET)
-	./$(TARGET_DIR)/$(TARGET)
+	@./$(TARGET_DIR)/$(TARGET)
 
 clean:
 	@rm $(TARGET_DIR)/*
 
 $(TARGET_DIR)/$(TARGET): $(SRC)
-	$(CC) -o $(TARGET_DIR)/$(TARGET) $(CFLAGS) -I$(INCLUDE) $(SRC)
+	$(CC) -o $(TARGET_DIR)/$(TARGET) $(DFLAGS) $(CFLAGS) -I$(INCLUDE) $(SRC)
--- src/main.c
+++ src/main.c
@@ -5,6 +5,14 @@
 #define TRUE 1 
 #define FALSE 0
 
+#ifdef DEBUG 
+  #define DEBUG_PRINT(x) fprintf(stderr, "DEBUG: "); fprintf(stderr,x);
+  #define DEBUG_PRINT2(x,y) fprintf(stderr, "DEBUG: "); fprintf(stderr,x,y);
+#else
+  #define DEBUG_PRINT(x) {}
+  #define DEBUG_PRINT2(x,y) {}
+#endif
+
 const uint32_t size_of_buffer_chunk = 1024;
 
 char * oshReadLine(void) 
@@ -24,17 +32,18 @@ char * oshReadLine(void)
     }
   }
 
+  DEBUG_PRINT2("Line read from the STDIN:\n\t%s", line)
   return line;
 }
 
 void oshLoop(void)
 {
   char * line; 
+  DEBUG_PRINT("Starting the main shell loop\n")
   while(1) 
   { 
     printf("osh > ");
     line = oshReadLine();
-    printf("%s", line);
     free(line);
   }
 }
@@ -43,10 +52,12 @@ void oshLoop(void)
 int main(int argc, char *argv[])
 {
   // Init 
-  
+  DEBUG_PRINT("Initialised the shell\n")
+    
   oshLoop();
 
   // Clean
+  DEBUG_PRINT("Cleaning and closing the shell\n")
   
   return EXIT_SUCCESS;
 }
