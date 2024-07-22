588fea0779647388fa0a5978584d30566f84db8e
DeriDreri <dominikcybulski@protonmail.com>
2024-07-22 12:48:19 +0200

    Refractor the codebase and implement small fixes
    - include/osh directory added for internal project header files
      and global.h added for projectwide variables and declarations
    - compile_commands.json generated by bear to allow clanglsp to
      properly find files for static code diagnostic
    - copyright added to main.c file
    - .gitignore updated with clangd cache directory

---
+++ include/osh/global.h
@@ -0,0 +1,15 @@
+#ifndef _OSH_GLOBAL_H
+#define _OSH_GLOBAL_H
+
+#define TRUE 1
+#define FALSE 0
+
+#ifdef DEBUG
+  #define DEBUG_PRINT(x) fprintf(stderr, "DEBUG> "); fprintf(stderr, x)
+  #define DEBUG_PRINT2(x, y) fprintf(stderr, "DEBUG> "); fprintf(stderr, x, y)
+#else
+  #define DEBUG_PRINT(x) {}
+  #define DEBUG_PRINT2(x, y) {}
+#endif
+
+#endif
--- src/main.c
+++ src/main.c
@@ -1,18 +1,13 @@
+/*
+ *   Copyright (c) 2024 Dominik Cybulski
+ *   All rights reserved.
+ */
 #include <stdint.h>
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
 
-#define TRUE 1
-#define FALSE 0
-
-#ifdef DEBUG
-  #define DEBUG_PRINT(x) fprintf(stderr, "DEBUG> "); fprintf(stderr, x)
-  #define DEBUG_PRINT2(x, y) fprintf(stderr, "DEBUG> "); fprintf(stderr, x, y)
-#else
-  #define DEBUG_PRINT(x) {}
-  #define DEBUG_PRINT2(x, y) {}
-#endif
+#include <osh/global.h>
 
 const uint32_t  tokenizer_buffer_chunk_size  = 64;
 const char *    tokenizer_delimeters         = " \t\r\n\a";
